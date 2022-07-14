3c68ad3348cf62a20c64748f19adcf8aed1fbd30
EC2 Default User <ec2-user@ip-172-31-23-157.ap-south-1.compute.internal>
2022-07-14 13:38:34 +0000

    App of Apps added

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -26,7 +26,34 @@ export default class PipelineConstruct extends Construct {
     .region(region)
     .addOns(...addOns)
     .teams(new TeamPlatform(account), new TeamApplication('backend',account));
-  
+    // HERE WE ADD THE ARGOCD APP OF APPS REPO INFORMATION
+    const repoUrl = 'https://github.com/codemato/eks-argo-workloads.git';
+
+    const bootstrapRepo : blueprints.ApplicationRepository = {
+        repoUrl,
+        targetRevision: 'trunk',
+    }
+
+    // HERE WE GENERATE THE ADDON CONFIGURATIONS
+    const devBootstrapArgo = new blueprints.ArgoCDAddOn({
+        bootstrapRepo: {
+            ...bootstrapRepo,
+            path: 'envs/dev'
+        },
+    });
+    const testBootstrapArgo = new blueprints.ArgoCDAddOn({
+        bootstrapRepo: {
+            ...bootstrapRepo,
+            path: 'envs/test'
+        },
+    });
+    const prodBootstrapArgo = new blueprints.ArgoCDAddOn({
+        bootstrapRepo: {
+            ...bootstrapRepo,
+            path: 'envs/prod'
+        },
+    });
+    
     blueprints.CodePipelineStack.builder()
       .name("eks-blueprints-accelerator-pipeline")
       .owner("codemato")
@@ -39,10 +66,10 @@ export default class PipelineConstruct extends Construct {
       .wave({
         id: "envs",
         stages: [
-          { id: "dev", stackBuilder: blueprint.clone('ap-south-1')},
-          { id: "test", stackBuilder: blueprint.clone('ap-southeast-1')},
-          { id: "prod", stackBuilder: blueprint.clone('ap-southeast-2')}
-        ]
+          { id: "dev", stackBuilder: blueprint.clone('ap-south-1').addOns(devBootstrapArgo)}, // HERE WE ADD OUR NEW ADDON WITH THE CONFIGURED ARGO CONFIGURATION
+          { id: "test", stackBuilder: blueprint.clone('ap-southeast-1').addOns(testBootstrapArgo)}, // HERE WE ADD OUR NEW ADDON WITH THE CONFIGURED ARGO CONFIGURATION
+          { id: "prod", stackBuilder: blueprint.clone('ap-southeast-2').addOns(prodBootstrapArgo)} // HERE WE ADD OUR NEW ADDON WITH THE CONFIGURED ARGO CONFIGURATION
+        ] 
       })
       .build(scope, id+'-stack', props);
   }
